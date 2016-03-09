47f46a82bad0b9afc6265893eb17004332c835a0
Nico <nicolaifsf@gmail.com>
2016-03-09 10:56:53 -0500

    Cleanup, made progress with ibm

--- google.go
+++ google.go
@@ -1,11 +1,18 @@
 package speech
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+)
+
+const (
+	key string = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"
+	url string = "https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=" + key
 )
 
 var googleKey string
@@ -38,10 +45,45 @@ func SendGoogleRequest(filePath string) {
 	fmt.Println(string(body))
 }
 
-//func GetGoogleCreds() (string, string) {
 func GetGoogleCreds() string {
 	return googleKey
 }
 func SetGoogleKey(key string) {
 	googleKey = key
+}
+
+func SendGoogleMessage(file string) string {
+	//stream, err := ioutil.ReadFile(file)
+	stream, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	info, err := stream.Stat()
+	if err != nil {
+		panic(err)
+	}
+	var size int64 = info.Size()
+	b := make([]byte, size)
+	buffer := bufio.NewReader(stream)
+	_, err = buffer.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	if err != nil {
+		panic(err)
+	}
+	//	req.Header.Set("Content-Type", "audio/l16; rate=44100;")
+	req.Header.Set("Content-Type", "audio/l16; rate=16000")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	bod, err := ioutil.ReadAll(res.Body)
+	return string(bod)
+
 }
--- ibm.go
+++ ibm.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"os"
 )
 
 var ibmUsername string
@@ -79,6 +81,7 @@ type IbmSeshJson struct {
 	Observe_result  string `json: "observe_result"`
 	Session_id      string `json: "session_id"`
 	New_session_uri string `json: "new_session_uri"`
+	Cookies         *cookiejar.Jar
 }
 
 /**
@@ -88,14 +91,26 @@ type IbmSeshJson struct {
 func GetSession() IbmSeshJson {
 	url := "stream.watsonplatform.net/speech-to-text/api/v1/sessions"
 	seshUrl := fmt.Sprintf("https://%s:%s@%s", ibmUsername, ibmPassword, url)
-	req, err := http.NewRequest("POST", seshUrl, nil)
+	jsonStr := []byte(`{}`)
+	req, err := http.NewRequest("POST", seshUrl, bytes.NewBuffer(jsonStr))
 	if err != nil {
 		log.Fatal(err)
 	}
-	client := &http.Client{}
+	req.Header.Set("Content-Type", "application/json")
+	cookiesJar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{
+		Jar: cookiesJar,
+	}
 	res, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 201 {
+		fmt.Println("WELL SHIT")
 	}
 	bod, err := ioutil.ReadAll(res.Body)
 	if err != nil {
@@ -109,5 +124,77 @@ func GetSession() IbmSeshJson {
 		fmt.Println("this err")
 		log.Fatal(err)
 	}
+	sesh.Cookies = cookiesJar
 	return sesh
+}
+
+func GetResults(sesh IbmSeshJson) {
+	results := sesh.Observe_result
+	client := &http.Client{
+		Jar: sesh.Cookies,
+	}
+	req, err := http.NewRequest("GET", results, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bod, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bod))
+}
+
+func SendIBMSpeech(fileRef string) {
+	sesh := GetSession()
+	seshURL := sesh.Recognize
+	client := &http.Client{
+		Jar: sesh.Cookies,
+	}
+	fmt.Println("seshURL = " + seshURL)
+	reader, err := os.Open(fileRef)
+	if err != nil {
+		log.Fatal("Error reading file:\n%v\n", err)
+	}
+
+	toURL := fmt.Sprintf("%s", seshURL)
+	fmt.Println("toURL : " + toURL)
+	req, err := http.NewRequest("POST", toURL, reader)
+	req.Header.Set("Authorization", fmt.Sprintf("Basic %s:%s", ibmUsername, ibmPassword))
+	req.Header.Set("Content-Type", "audio/wav")
+	req.Header.Set("Transfer-Encoding", "Chunked")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bod, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bod))
+}
+func ReceiveSpeech(s *IbmSeshJson) {
+	seshURL := s.Observe_result
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", seshURL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Basic %s:%s", ibmUsername, ibmPassword))
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bod, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bod))
 }
